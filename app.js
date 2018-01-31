75ea3aeac08bf849db6fa9e49dd2ea45a063f2c2
Jen Lipton <jkl.lipton@gmail.com>
2018-01-31 14:57:07 -0800

    Added Guessing Game question, arrays

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// Define YN arrays
+const yes = ['yes', 'y', 'yeah', 'yes!', 'sure'];
+const no = ['no', 'n', 'nah', 'nope', 'no!',];
+
 // Welcome
 
-let userName = prompt('Hey, let\'s chat!  What do you want me to call you?');
+let userName = prompt('Hey, let\'s chat! What should I call you?');
 if (userName == false) {
     do {
         userName = prompt('Come on, give me a name!  Any name!');
@@ -12,6 +16,18 @@ if (userName == false) {
 alert('Hey, ' + userName + ', nice to meet you!  I\'m Jen.');
 console.log('Visitor\'s name is: ' + userName);
 
-// Define arrays
-const yes = ['yes', 'y', 'yeah', 'yes!', 'sure'];
-const no = ['no', 'n', 'nah', 'nope', 'no!',];
+//Guessing Game?
+const gameYN = prompt('You want to play a guessing game?').toLowerCase();
+console.log(userName + ' said ' + gameYN + ' to playing.');
+if (yes.includes(gameYN)) {
+    alert('Yay!');
+} else if (no.includes(gameYN)) {
+    alert('What? Of course you do!');
+} else {
+    alert('Let\'s play with Yes or No answers.');
+}
+
+//Questions 2-5
+
+const questions = ['What should I call you?',];
+const answers = [];
