44481dc51f502207b6f572a3be291458fc5be208
Jen Lipton <jkl.lipton@gmail.com>
2018-01-31 15:37:39 -0800

    Updated name question to respond to null

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const yes = ['yes', 'y', 'yeah', 'yes!', 'sure'];
 const no = ['no', 'n', 'nah', 'nope', 'no!',];
 
 // Welcome
-
 let userName = prompt('Hey, let\'s chat! What should I call you?');
-if (userName == false) {
-    do {
-        userName = prompt('Come on, give me a name!  Any name!');
-
-    } while (userName == false);
+for (let i = 0; i <= 3; i++) {
+    if ((userName == false || userName == '') && i < 3) {
+        userName = prompt('Give me a name!  Any name!');
+    } else if ((userName == false || userName == '') && i === 3){
+        alert('Okay, smartie.  I\'ll just call you Meryl.');
+        userName = 'Meryl';
+        break;
+    } else {
+        alert('Hi, ' + userName + ', nice to meet you!  My name is Jen.');
+    }
 }
-alert('Hey, ' + userName + ', nice to meet you!  I\'m Jen.');
 console.log('Visitor\'s name is: ' + userName);
 
 //Guessing Game?
@@ -31,3 +34,5 @@ if (yes.includes(gameYN)) {
 
 const questions = ['What should I call you?',];
 const answers = [];
+const wrong = [];
+const right = [];
